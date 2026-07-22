6e89b454fcf0bb7afea67d8b4f6f1ebce024ed85
Cameron Brooks <cambrooks3393@gmail.com>
2026-07-21 23:03:02 -0400

    fix(variants): restore Z_MAX_POS to 210
    Z clearance tested on the pellet toolhead; full 210 mm height is
    reachable, so revert the conservative 207 back to the stock value in
    both variants.

--- Firmware/variants/MK3.h
+++ Firmware/variants/MK3.h
@@ -62,7 +62,7 @@
 #define X_MIN_POS 0
 #define Y_MAX_POS 212.5
 #define Y_MIN_POS -4 //orig -4
-#define Z_MAX_POS 207 // !!!CHANGED: 210 to 207 for GB3DPE
+#define Z_MAX_POS 210
 #define Z_MIN_POS 0.15
 
 // Z height correction value
